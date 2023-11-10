5c3dd2fefe5ef763cf0952f19f88a86c4df737d6
Yosakk <bagasbrawijaya100@gmail.com>
2023-11-10 14:35:06 +0700

    intl&uuid&barcode

--- lib/view/pdf_view.dart
+++ lib/view/pdf_view.dart
@@ -1,21 +1,22 @@
-
 import 'dart:typed_data';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:uuid/uuid.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 import 'package:ugd6_b_9/view/preview_screenforPDF.dart';
 import 'package:ugd6_b_9/entity/gerakanGym.dart';
+import 'package:intl/intl.dart';
 
-
-Future<pw.Document> generateDocument(List<Map<String, dynamic>> gerakanGym) async {
-
-  final logo= (await rootBundle.load('assets/logo.png')).buffer.asUint8List();
+Future<pw.Document> generateDocument(
+    List<Map<String, dynamic>> gerakanGym) async {
+  final logo = (await rootBundle.load('assets/logo.png')).buffer.asUint8List();
   final image = pw.MemoryImage(logo);
-
+ 
+  String id = const Uuid().v1();
   final doc = pw.Document();
 
   doc.addPage(pw.Page(
@@ -25,7 +26,6 @@ Future<pw.Document> generateDocument(List<Map<String, dynamic>> gerakanGym) asyn
           child: pw.Column(
             mainAxisAlignment: pw.MainAxisAlignment.start,
             children: [
-
               pw.Padding(
                 padding: pw.EdgeInsets.all(10),
                 child: pw.Text(
@@ -38,13 +38,15 @@ Future<pw.Document> generateDocument(List<Map<String, dynamic>> gerakanGym) asyn
               ),
               pw.Padding(
                 padding: pw.EdgeInsets.all(10),
-                child: pw.Image(image,
-                  width: 150, height: 150,
+                child: pw.Image(
+                  image,
+                  width: 150,
+                  height: 150,
                 ),
               ),
-
               pw.Table(
-                border: pw.TableBorder.all(width: 2,style: pw.BorderStyle.solid),
+                border:
+                    pw.TableBorder.all(width: 2, style: pw.BorderStyle.solid),
                 tableWidth: pw.TableWidth.max,
                 children: [
                   pw.TableRow(children: [
@@ -89,8 +91,9 @@ Future<pw.Document> generateDocument(List<Map<String, dynamic>> gerakanGym) asyn
               ),
               pw.Padding(
                 padding: pw.EdgeInsets.all(10),
-                child: footerPdf(DateTime.now().toString()),
-              )
+                child: footerPdf(DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now()).toString()),
+              ),
+              barcodeKotak(id),
             ],
           ),
         );
@@ -99,9 +102,9 @@ Future<pw.Document> generateDocument(List<Map<String, dynamic>> gerakanGym) asyn
 }
 
 Widget PaddedText(
-    final String text, {
-      final TextAlign align = TextAlign.left,
-    }) =>
+  final String text, {
+  final TextAlign align = TextAlign.left,
+}) =>
     Padding(
       padding: EdgeInsets.all(10),
       child: Text(
@@ -110,7 +113,26 @@ Widget PaddedText(
       ),
     );
 
-
 pw.Center footerPdf(String formattedDate) => pw.Center(
-    child: pw.Text('Created At $formattedDate',
-        style: pw.TextStyle(fontSize: 10, color: PdfColors.black)));
+      child: pw.Text(
+        'Created At $formattedDate',
+        style: pw.TextStyle(fontSize: 10, color: PdfColors.black),
+      ),
+    );
+
+
+pw.Padding barcodeKotak(String id) {
+  return pw.Padding(
+    padding: pw.EdgeInsets.symmetric(horizontal: 100, vertical: 100),
+    child: pw.Center(
+      child: pw.BarcodeWidget(
+        barcode: pw.Barcode.qrCode(
+          errorCorrectLevel: BarcodeQRCorrectionLevel.high,
+        ),
+        data: id,
+        width: 150,
+        height: 150,
+      ),
+    ),
+  );
+}
